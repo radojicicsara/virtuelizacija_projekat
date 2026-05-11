92e660d0cc0cf7b3a9064a6feec5be51d9baddf9
Sara Radojičić <sararadojicic.sri@gmail.com>
2026-05-11 15:52:19 +0200

    Dodati modeli podataka i servisni interfejs u Common projekat

---
+++ BaterijaProjekat/Common/EisMeta.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace Common
+{
+    [DataContract]
+    public class EisMeta
+    {
+        [DataMember]
+        public string BatteryId { get; set; } // npr. B01, B02...
+
+        [DataMember]
+        public string TestId { get; set; }    // npr. Test_1
+
+        [DataMember]
+        public int SoC { get; set; }          // State of Charge (npr. 50, 100)
+
+        [DataMember]
+        public string FileName { get; set; }  // Naziv originalnog fajla
+
+        [DataMember]
+        public int TotalRows { get; set; }    // Koliko redova klijent planira da pošalje
+    }
+}
---
+++ BaterijaProjekat/Common/EisSample.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Common
+{
+    [DataContract]
+    public class EisSample
+    {
+        [DataMember] public int RowIndex { get; set; }
+        [DataMember] public double FrequencyHz { get; set; }
+        [DataMember] public double R_ohm { get; set; }
+        [DataMember] public double X_ohm { get; set; }
+        [DataMember] public double T_degC { get; set; }
+        [DataMember] public double Range_ohm { get; set; }
+        [DataMember] public DateTime TimestampLocal { get; set; }
+    }
+}
